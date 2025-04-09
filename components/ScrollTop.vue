3562b06151fb877051c9e5e2ad3b04961eda6e44
NguefackLoic <Lnguefack11@gmail.com>
2025-04-09 12:39:14 +0100

    update scroll to top

--- components/ScrollTop.vue
+++ components/ScrollTop.vue
@@ -2,9 +2,13 @@
     <button
       v-show="visible"
       @click="scrollToTop"
-      class="fixed bottom-6 right-6 bg-[#581845] text-white p-3 rounded-full shadow-lg hover:bg-[#3a112e] transition-all z-50"
+      class="fixed bottom-6 right-6 bg-[#581845] text-white p-3 rounded-full shadow-lg hover:opacity-90 transition-all duration-300 z-50"
+      aria-label="Scroll to top"
     >
-      ⬆️
+      <!-- Icône propre sans fond coloré autour -->
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+      </svg>
     </button>
   </template>
   
