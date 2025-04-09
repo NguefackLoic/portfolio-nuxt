91d8220b877a56212b7f9269384b9cc72d4b7c07
NguefackLoic <Lnguefack11@gmail.com>
2025-04-09 12:46:49 +0100

    update service card

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div  class=" bg-white dark:bg-gray-900 rounded-2xl p-6 shadow hover:shadow-lg transition-all duration-300">
+    <div  class="flex flex-col justify-center text-center items-center bg-white dark:bg-gray-900 rounded-2xl p-6 shadow hover:shadow-lg transition-all duration-300">
       <div class="text-4xl mb-4">{{ icon }}</div>
       <h3 class="text-xl line-1 anim-typewriter text-shadow font-semibold text-gray-800 dark:text-gray-300 mb-2">{{ title }}</h3>
       <p class="text-gray-700 dark:text-gray-300">{{ description }}</p>
@@ -7,7 +7,7 @@
   </template>
   
   <script setup>
-  defineProps({
+  defineProps({   
     icon: String,
     title: String,
     description: String,
